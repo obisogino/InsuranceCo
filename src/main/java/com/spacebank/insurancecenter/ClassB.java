123c5e7e2c918485c39f48e4c4e1089386dd2939
Gino <--->
2022-10-12 13:33:36 +0800

    Implement create user, get user, login user

---
+++ src/main/java/com/spacebank/insurancecenter/ClassB.java
@@ -0,0 +1,27 @@
+package com.spacebank.insurancecenter;
+
+public class ClassB {
+	String str;
+	Integer inte;
+	
+	public ClassB(String str, Integer inte) {
+		this.inte = inte;
+		this.str = str;
+	}
+	
+	public String getStr() {
+		return str;
+	}
+	
+	public Integer getInte() {
+		return inte;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+
+	public void setInte(Integer inte) {
+		this.inte = inte;
+	}
+}
